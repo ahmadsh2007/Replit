4f59816bbdb40b64b574a49fb6f6e5d206739821
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2024-12-07 15:01:27 +0300

    Started Building an Desktop Application for Database and SQL

--- Course Desktop Application/MainProgram.py
+++ Course Desktop Application/MainProgram.py
@@ -2,24 +2,40 @@ import tkinter as tk
 import sys
 import os
 
+from DatabaseHandler import DatabaseHandler
+from RegistrationForm import RegeristrationForm
+
 def currentDirectory():
-    # Get the directory of the current script
+    """
+    Return the current directory of this script and add it to the system path.
+
+    The reason for doing this is because the SQL database is stored in a
+    relative path from the location of this script. This function is used to
+    get the current location of the script and add it to the system path so
+    we can import the database from the relative path.
+
+    Returns:
+        None
+    """
     scriptDir: str = os.path.dirname(os.path.abspath(__file__))
 
     # Add this directory to the system path
     return sys.path.append(scriptDir)
 
+
+
 class MainApplication(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Student Manangement System")
+        self.title("Student Management System")
         self.geometry("900x600")
         self.createWidgets()
 
     def createWidgets(self):
-        titleLabel = tk.Label(self, text="Student Manangement System", font=("Helvetica", 10))
+        titleLabel = tk.Label(self, text="Student Management System", font=("Helvetica", 10))
         titleLabel.pack(side='top', fill='x')
 
+        self.registrationForm = RegeristrationForm()
 
 
 
@@ -27,3 +43,5 @@ if __name__ == "__main__":
     currentDirectory()
     app = MainApplication()
     app.mainloop()
+    db = DatabaseHandler()
+    print(db.fetchAllStudents())
