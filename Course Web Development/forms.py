ee2129841a5cb52d24a5fc1d7216f1f836c85695
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2024-12-22 15:18:29 +0300

    Form Inputs Added

--- Course Web Development/forms.py
+++ Course Web Development/forms.py
@@ -1,4 +1,9 @@
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 class EventForm(FlaskForm):
-    pass
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+
+    submit = SubmitField('Create Event')
